a9c934144807f85bbe62fb982fbe0f9fc1a867db
Ritik Nandanwar <nandanwarritik10@gmail.com>
2022-08-08 01:02:38 +0530

    conditional rendering of chat screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import Chat from "./Components/Chat";
 import io from "socket.io-client";
 const socket = io.connect("http://localhost:3001");
 function App() {
   const [username, setUsername] = useState("");
   const [roomID, setRoomID] = useState("");
+  const [showChat, setShowChat] = useState(false);
   const [msg, setMsg] = useState("");
   const joinRoom = () => {
     const data = {
@@ -11,6 +13,7 @@ function App() {
       roomID,
     };
     socket.emit("joinRoom", data);
+    setShowChat(true);
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,46 +32,50 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col mx-auto max-w-[400px] text-center justify-center p-8 ">
-        <input
-          type="text"
-          placeholder="Enter username"
-          className="px-3 py-3 m-2 bg-slate-500 font-bold"
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Enter roomID"
-          className="px-3 py-3 m-2 bg-slate-500 font-bold"
-          onChange={(e) => {
-            setRoomID(e.target.value);
-          }}
-        />
-        <button
-          className="p-3 m-2 bg-black/70 capitalize font-bold"
-          onClick={joinRoom}
-        >
-          PRESS
-        </button>
-        <form onSubmit={handleSubmit}>
+      {!showChat ? (
+        <div className="flex flex-col mx-auto max-w-[400px] text-center justify-center p-8 ">
           <input
             type="text"
-            placeholder="Enter message"
-            className="mt-20 px-3 py-3 m-2 font-bold bg-fuchsia-500"
+            placeholder="Enter username"
+            className="px-3 py-3 m-2 bg-slate-500 font-bold"
             onChange={(e) => {
-              setMsg(e.target.value);
+              setUsername(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Enter roomID"
+            className="px-3 py-3 m-2 bg-slate-500 font-bold"
+            onChange={(e) => {
+              setRoomID(e.target.value);
             }}
           />
           <button
-            type="submit"
-            className="m-2 py-3 px-4 bg-green-400 capitalize font-bold"
+            className="p-3 m-2 bg-black/70 capitalize font-bold"
+            onClick={joinRoom}
           >
-            Send
+            PRESS
           </button>
-        </form>
-      </div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter message"
+              className="mt-20 px-3 py-3 m-2 font-bold bg-fuchsia-500"
+              onChange={(e) => {
+                setMsg(e.target.value);
+              }}
+            />
+            <button
+              type="submit"
+              className="m-2 py-3 px-4 bg-green-400 capitalize font-bold"
+            >
+              Send
+            </button>
+          </form>
+        </div>
+      ) : (
+        <Chat />
+      )}
     </>
   );
 }
